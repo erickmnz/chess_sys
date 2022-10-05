c7999ceb4c86d032173438d4b3a21376b150fd45
erickmnz <ericklmenezes@outlook.com>
2022-10-05 14:07:48 -0300

    placing first pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8,8);
+		iSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,5 +22,15 @@ public class ChessMatch {
 			
 		}
 		return mat;
+	}
+	
+	public void iSetup() {
+		for (int i = 0; i<8; i++) {
+			board.placePiece(new Rook(board, Color.WHITE), new Position(2,i));
+			board.placePiece(new Rook(board, Color.BLACK), new Position(5,i));
+
+		}
+		board.placePiece(new King(board, Color.WHITE), new Position(0,4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7,3));
 	}
 }
