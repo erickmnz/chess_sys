0e8f946508ec3d745a291878e617e375356925f3
erickmnz <ericklmenezes@outlook.com>
2022-10-05 13:43:12 -0300

    board logic

--- src/app/main.java
+++ src/app/main.java
@@ -1,9 +1,13 @@
 package app;
 
+import chess.ChessMatch;
+
 public class main {
 
 	public static void main(String[] args) {
-
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
